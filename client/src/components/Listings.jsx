af35ead4e834cca93eede4559f5345f39ca249fc
John Anderton <janderton24@gmail.com>
2020-11-20 14:02:22 -0800

    removes commented-out pieces of code

--- client/src/components/Listings.jsx
+++ client/src/components/Listings.jsx
@@ -4,14 +4,6 @@ import styled from 'styled-components';
 import Listing from './Listing.jsx';
 import { ListingsBody } from './StyledComponents.jsx';
 
-// const ListingsBody = styled.div`
-// grid-area: listings;
-// overflow-x: hidden;
-// overflow-y: hidden;
-// display: flex;
-// justify-content: space-between;
-// background: white;
-// `;
 
 const Listings = ({ listingData, clickStar, starred, firstImg }) => {
   return (
@@ -26,36 +18,3 @@ const Listings = ({ listingData, clickStar, starred, firstImg }) => {
 }
 
 export default Listings;
-
-/////////////////////////////////////////////////////////////////////////////
-///////ORGINAL/////////////////////
-/////////////////////////////////////////////////////////////////////////////
-
-
-// import React, { useState, useEffect } from 'react';
-// import ReactDOM from 'react-dom';
-// import styled from 'styled-components';
-// import Listing from './Listing.jsx';
-
-// const ListingsBody = styled.div`
-// grid-area: listings;
-// overflow-x: hidden;
-// overflow-y: hidden;
-// display: flex;
-// justify-content: space-between;
-// background: white;
-// `;
-
-// const Listings = ({ listingData, clickStar, starred, firstImg }) => {
-//   return (
-//     <ListingsBody>
-//       {
-//         listingData.map((listing, index) => {
-//           return <Listing listing = {listing} clickStar = { clickStar } starred = { starred } id = {firstImg + index}/>
-//       })
-//       }
-//     </ListingsBody>
-//   )
-// }
-
-// export default Listings;
