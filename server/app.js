0846386ce5ccff723f9737c6fce73e0151b2bfd9
John Anderton <janderton24@gmail.com>
2020-11-21 10:54:27 -0800

    removes html file from git ignore file

--- server/app.js
+++ server/app.js
@@ -10,7 +10,10 @@ app.use(bodyParser.json());
 
 app.get('/api/nearbyHomes', (req, res) => {
   router.getAllHomesFromDB((err, result) => {
-    if (err) throw err;
+    if (err) {
+      console.log('error: ', error);
+      res.send('error occurred');
+    }
     res.send(result);
   })
 })
@@ -18,7 +21,10 @@ app.get('/api/nearbyHomes', (req, res) => {
 app.get('/api/nearbyHomes/:id', (req, res) => {
   const ID = Number(req.params.id);
   router.getNearbyFromID(ID, (err, result) => {
-    if (err) throw err;
+    if (err) {
+      console.log('error: ', error);
+      res.send('error occurred');
+    }
     router.getCityHomesFromDB(result[0].city, ID, (err, results) => {
           if (err) throw err;
           res.send(results);
