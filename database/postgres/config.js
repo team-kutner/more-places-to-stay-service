94e540dfa0669ce874554e4e73f3946472a913fb
Mitch McDermott <mmcdermott319@gmail.com>
2020-12-14 17:12:52 -0600

    Postgres config updated

--- database/postgres/config.js
+++ database/postgres/config.js
@@ -5,5 +5,5 @@ module.exports = {
   database: 'homesservice',
   port: 5432,
   // eslint-disable-next-line camelcase
-  db_URI: 'postgres://mitchmcdermott:mitch@localhost:5432/homesservice'
+  db_URI: 'postgres://postgres:mitch@localhost:5432/homesservice'
 };
